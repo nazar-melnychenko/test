9741f2be1e025599667f21e2da417e8778cd393b
Nazar Melnychenko <nazar.melnychenko@gmail.com>
2020-07-07 15:04:51 +0300

    Updates

---
+++ precache-manifest.956952bdf4ba17ea411acb8dfb34513e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2828fbf275ccf614a43d40e9f906636b",
+    "url": "/27_nerds_test/index.html"
+  },
+  {
+    "revision": "100c1dd4d1efeaa4cd4b",
+    "url": "/27_nerds_test/static/css/main.7b7c0b4c.chunk.css"
+  },
+  {
+    "revision": "ae2d28edfaf63cb4c2e9",
+    "url": "/27_nerds_test/static/js/2.4852b7f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/27_nerds_test/static/js/2.4852b7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100c1dd4d1efeaa4cd4b",
+    "url": "/27_nerds_test/static/js/main.fc4a2a9f.chunk.js"
+  },
+  {
+    "revision": "b541f6709d47122beada",
+    "url": "/27_nerds_test/static/js/runtime-main.1e5be380.js"
+  }
+]);
